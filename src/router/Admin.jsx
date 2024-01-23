26bb8118c9c7e8d0c03ece9092b7e6b5a24163b7
unknown <xakimovotabek200>
2024-01-23 14:01:09 +0500

    add remove bugs

--- src/router/Admin.jsx
+++ src/router/Admin.jsx
@@ -1,15 +1,25 @@
-import { Button } from "@mantine/core";
-import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Burger, VisuallyHidden } from "@mantine/core";
+import {
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { Logo_text } from "../assets/images";
 import { ADMIN } from "../constants";
-import { useLocation } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
 
 const Admin = () => {
-  let open = true;
   const location = useLocation();
+  const [opened, { toggle }] = useDisclosure();
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
   const isMenuItemActive = (path) => {
-    return location.pathname === path;
+    const isActive = location.pathname === path;
+    console.log(`Path: ${path}, isActive: ${isActive}`);
+    return isActive;
   };
 
   const LogoutComponent = () => {
@@ -24,20 +34,24 @@ const Admin = () => {
 
   return (
     <div className="flex">
-      <div className={`w-72 border-r h-screen pt-8 `}>
+      <div className={`w-72 border-r h-screen pt-8 md:block hidden`}>
         <div className="flex gap-x-4 items-center">
           <img src={Logo_text} className={`cursor-pointer w-fit `} />
         </div>
-        <ul className="pt-6">
+        <ul className="pt-6 ">
           {ADMIN.map((Menu, index) => (
             <NavLink
               to={Menu.path}
               key={index}
               className={`flex rounded-md p-2 cursor-pointer items-center ${
-                isMenuItemActive(Menu.path) ? "activeButton" : ""
+                isMenuItemActive(Menu.path) ? "activeButton" : "default"
               }`}
             >
-              <Button fullWidth variant="default">
+              <Button
+                fullWidth
+                variant="default"
+                // className={}
+              >
                 <span className="text-xl">{Menu.icon}</span>
                 <p className="text-xl font-semibold pl-4">{Menu.title}</p>
               </Button>
@@ -61,6 +75,49 @@ const Admin = () => {
           ))}
         </Routes>
       </div>
+
+      <Burger
+        opened={toggle}
+        className="md:hidden"
+        onClick={() => setBurgerMenuOpen(!burgerMenuOpen)}
+        aria-label="Toggle navigation"
+      />
+
+      {burgerMenuOpen && (
+        <div className="bg-gray-800 text-white fixed top-0  w-64 h-screen">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-x-4 items-center">
+              <img src={Logo_text} className={`cursor-pointer w-fit `} />
+            </div>
+            {/* burger items */}
+            <ul className="pt-6 ">
+              {ADMIN.map((Menu, index) => (
+                <NavLink
+                  to={Menu.path}
+                  key={index}
+                  className={`flex rounded-md p-2 cursor-pointer items-center ${
+                    isMenuItemActive(Menu.path) ? "activeButton" : ""
+                  }`}
+                >
+                  <Button fullWidth variant="default">
+                    <span className="text-xl">{Menu.icon}</span>
+                    <p className="text-xl font-semibold pl-4">{Menu.title}</p>
+                  </Button>
+                </NavLink>
+              ))}
+              <Button
+                fullWidth
+                color="red"
+                className="hidden items-center  bg-red-500 my-5 text-white xl:flex"
+                onClick={LogoutComponent}
+              >
+                <i className="fa-solid fa-arrow-left  items-center text-xl text-white" />
+                <p className="pl-5 text-xl  font-semibold"> Log out</p>
+              </Button>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
