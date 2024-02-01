1053c5068de368c12d606a51fae1c4db7df8ed98
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-02-01 13:18:36 +0500

    library page UI

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,7 @@ import Logo from "./Logo-removebg-preview.png";
 import Logo_text from "./Logo_text.png";
 import Logo_icon from "./Logo-icon.png";
 import News from "./news.jpg";
+import Pdf from "./pdf.png";
 import ShowcaseBg from "./showcase-bg.jpg";
 
-export { LoginBg, Logo, Logo_text, Logo_icon, News, ShowcaseBg };
+export { LoginBg, Logo, Logo_text, Logo_icon, News, Pdf, ShowcaseBg };
