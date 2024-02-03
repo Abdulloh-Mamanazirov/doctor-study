f6240c9efbd11ce4bfeac3f8931021db764b997a
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-02-03 14:29:22 +0500

    rename image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,7 +4,7 @@ import Logo_text from "./Logo_text.png";
 import Logo_icon from "./Logo-icon.png";
 import News from "./news.jpg";
 import Pdf from "./pdf.png";
-import PdfId from "./PdfId.png";
+import PdfId from "./pdfId.png";
 import ShowcaseBg from "./showcase-bg.jpg";
 
 export { LoginBg, Logo, Logo_text, Logo_icon, News, Pdf, PdfId, ShowcaseBg };
